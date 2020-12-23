6bd7a5b4362a19717dd0cba9952de37cfe8247f2
mi2507 <mreginadasilva@yahoo.com.br>
2020-12-23 14:11:36 -0300

    efetuado alteracao no arquivo App.css para inclusao de estilo p versao desktop e mobile e arquivo App.js para inclusao de texto e imagem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
+import profile from './profile.jpeg';
+import logoelo from './logoelo.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <img src={logoelo} className="App-logoelo" alt="logotipo da elo"></img>
         <img src={logo} className="App-logo" alt="logo" />
+        <p> 
+          Este App foi feito por Michelle Regina
+          </p>
+          <img src={profile} className="App-profile" alt="minha foto de perfil" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
